da146d7ee9c92843ebca14fdd090d17a14f51c31
Fabien Dijaud <fabien.dijaud@posteo.net>
2025-03-25 12:13:15 +0100

    feat: use environment variables for Firebase configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAfGueljLagS1qfhECBtGfu7qt-2Ahmmj8",
-  authDomain: "travo-reno-dev.firebaseapp.com",
-  projectId: "travo-reno-dev",
-  storageBucket: "travo-reno-dev.appspot.com",
-  messagingSenderId: "562417085313",
-  appId: "1:562417085313:web:6546896e4457dc1e7b9680"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
